557d8e4a09c228c3e2b7e2a308e710c44eda08b5
bhargav50 <bhargavsangani50@gmail.com>
2019-04-17 21:50:46 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import json1 from '../../JSON/level1'
+import json2 from '../../JSON/level2'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Task';
+  public json1: any = json1
+  public json2: any = json2
+  json1keys = Object.keys(json1);
+  // Mapjson1 = new Map(this.json1)
+  // Mapjson2 = new Map(this.json2)
+
+  showChildrenContent: Boolean = false
+  ngOnInit() {
+    console.log('JSON1: ', json1.children);
+  }
+  spreadChildren() {
+    this.showChildrenContent = true
+  }
 }
